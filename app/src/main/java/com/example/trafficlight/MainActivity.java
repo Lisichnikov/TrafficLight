3cfb0a41916d99c10a2622ff045cdbe18e522371
Алексей Лисичников <alexochanel@gmail.com>
2023-03-18 18:58:35 +0300

    переделал бархатные тяги

--- app/src/main/java/com/example/trafficlight/MainActivity.java
+++ app/src/main/java/com/example/trafficlight/MainActivity.java
@@ -5,42 +5,32 @@ import android.app.Activity;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
-    public TextView tvInfo;
-    public RelativeLayout relativeLayout;
+    public LinearLayout linearLayout;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        relativeLayout = (RelativeLayout)findViewById(R.id.relativelayout);
-        tvInfo = (TextView)findViewById(R.id.textView1);
-
-        Button buttonYellow = (Button)findViewById(R.id.buttonYellow);
-
-        buttonYellow.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                tvInfo.setText(R.string.yellow);
-                relativeLayout.setBackgroundResource(R.color.yellowColor);
-            }
-        });
+        linearLayout = (LinearLayout) findViewById(R.id.linearlayout);
     }
 
     public void buttonRed_Click(View v)
     {
-        tvInfo.setText(R.string.red);
-        relativeLayout.setBackgroundResource(R.color.redColor);
+        linearLayout.setBackgroundResource(R.color.redColor);
     }
-
     public void buttonGreen_Click(View v)
     {
-        tvInfo.setText(R.string.green);
-        relativeLayout.setBackgroundResource(R.color.greenColor);
+        linearLayout.setBackgroundResource(R.color.greenColor);
+    }
+     public void buttonYellow_Click(View v)
+    {
+        linearLayout.setBackgroundResource(R.color.yellowColor);
     }
 }
